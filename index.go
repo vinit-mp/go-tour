e5020f3ed65bb81ca84903e10872311c889083ec
vinit <vinitmp.work@gmail.com>
2024-11-24 19:49:41 +0530

    add functions with string interpolation

--- index.go
+++ index.go
@@ -1,5 +1,17 @@
 package main
 
+import _ "math/rand"
+
 func main() {
 	println("Hello World !")
+
+	println("%v ", sum(4, 5))
+
+}
+
+/* NOTE: Unlike c ++ and dart in this language the datatype declaration succeeds the variable declaration.  and the return type preseeds the function lambda.  */
+
+func sum(x int, y int) int {
+
+	return x + y
 }
